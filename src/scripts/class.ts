30cdf823915971e0f43c88af093fdf7c85c63213
arayutw <125749298+arayutw@users.noreply.github.com>
2023-04-07 11:08:13 +0900

    Updated the class.

---
+++ src/scripts/class.ts
@@ -0,0 +1,48 @@
+type Type = string | number | symbol
+
+type Handler<T1, T2, T3> = (event: {
+  target: T1
+  type: T2
+} & T3) => any
+
+type Options = {
+  once: boolean
+}
+
+export default class Emitter<T1, T2 extends Record<Type, { [key: string]: any }>> {
+  private readonly Emitter$items: [
+    name: Type,
+    listener: any,
+    options?: Options
+  ][] = []
+
+  public on<U extends keyof T2>(name: U, handler: Handler<T1, U, T2[U]>, options?: Options): void {
+    this.off(name, handler);
+    this.Emitter$items.push([name, handler, {
+      once: !!options?.once,
+    },]);
+  }
+
+  public off<U extends keyof T2 | "*">(name: U, handler?: Handler<T1, U, T2[U]>): void {
+
+    for (let a = this.Emitter$items, i = 0; a.length > i; i++) {
+      if ("*" === name || (a[i][0] === name && a[i][1] === handler)) {
+        a.splice(i--, 1);
+      }
+    }
+  }
+
+  public emit<U extends keyof T2>(name: U, event: T2[U]): void {
+    this.Emitter$items.forEach((entry) => {
+      if (name === entry[0]) {
+        entry[1]({
+          ...event,
+          target: this,
+          type: name,
+        });
+
+        if (entry[2]?.once) this.off(name, entry[1]);
+      }
+    });
+  }
+}
