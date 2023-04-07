4460420986da2be093bc519470d0afb5365ef687
arayutw <125749298+arayutw@users.noreply.github.com>
2023-04-07 10:43:06 +0900

    Update index.d.ts

--- dist/scripts/index.d.ts
+++ dist/scripts/index.d.ts
@@ -6,12 +6,12 @@ type Handler<T1, T2, T3> = (event: {
 type Options = {
     once: boolean;
 };
-export declare class Emitter<T1, T2 extends Record<Type, {
+export default class Emitter<T1, T2 extends Record<Type, {
     [key: string]: any;
 }>> {
     private readonly Emitter$items;
     on<U extends keyof T2>(name: U, handler: Handler<T1, U, T2[U]>, options?: Options): void;
-    off<U extends keyof T2>(name: U, handler: Handler<T1, U, T2[U]>): void;
+    off<U extends keyof T2 | "*">(name: U, handler?: Handler<T1, U, T2[U]>): void;
     emit<U extends keyof T2>(name: U, event: T2[U]): void;
 }
 export {};
